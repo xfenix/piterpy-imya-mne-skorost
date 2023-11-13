c02a344e74bf86b4523ab2231a87221522cfa079
Denis Anikin <ad@xfenix.ru>
2023-11-13 09:52:23 +0300

    Some final fixes

--- test-post.js
+++ test-post.js
@@ -1,9 +1,15 @@
 import http from "k6/http";
 import { sleep } from "k6";
-import { Faker } from "@faker-js/faker";
 
 const POSSIBE_DASHBOARD_TYPES = [1, 2, 3];
-const fakerObj = new Faker();
+const genRandomNumber = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+const genRandomDate = (from = new Date(2023, 0, 1), to = new Date()) => {
+  return new Date(
+    from.getTime() + Math.random() * (to.getTime() - from.getTime())
+  );
+};
 export const options = {
   stages: [
     { duration: "20s", target: 100 },
@@ -16,14 +22,14 @@ export default function () {
   http.post(
     "http://37.230.112.113/test/complex/",
     JSON.stringify({
-      user_name: fakerObj.person.firstName(),
-      sound_volume: fakerObj.rangeToNumber({ min: 10, max: 100 }),
-      score: fakerObj.rangeToNumber({ min: 0, max: 10 ^ 10 }),
+      user_name: (Math.random() + 1).toString(36).substring(20),
+      sound_volume: genRandomNumber(10, 100),
+      score: genRandomNumber(0, 10 ^ 10),
       type_of_dashboard:
         POSSIBE_DASHBOARD_TYPES[
           Math.floor(Math.random() * POSSIBE_DASHBOARD_TYPES.length)
         ],
-      when: fakerObj.date.recent().toISOString(),
+      when: genRandomDate().toISOString(),
     }),
     {
       headers: { "Content-Type": "application/json" },
